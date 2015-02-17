a9c6ed2240ba0375970c4e89ccc19b0c433bc67b
Sergei Matheson <sergei.matheson@gmail.com>
2015-02-18 10:17:54 +1100

    Handle -h in shell wrapper

--- bash/shellject_wrapper.sh
+++ bash/shellject_wrapper.sh
@@ -4,12 +4,12 @@ shellject ()
   executable=$( type -P shellject );
   output=$( $executable $@ );
   shellject_exit=$?;
-  if [[ $shellject_exit == 0 && "$1" == "load" && ! $@ =~ "--help" ]]; then
+  if [[ $shellject_exit == 0 && "$1" == "load" && ! ($@ =~ "--help" || $@ =~ " -h") ]]; then
     eval "$output";
     eval_exit=$?
     if [[ $eval_exit != 0 ]]; then
       echo "Loading shelljection $2 FAILED";
-      return $eval_exit
+      return $eval_exit;
     fi;
   else
     if [[ $shellject_exit == 0 ]]; then
