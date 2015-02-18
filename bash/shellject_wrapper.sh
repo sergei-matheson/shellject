59e29b398c16e19698a7c13be1389787f5d0a641
Sergei Matheson <sergei.matheson@gmail.com>
2015-02-19 10:27:46 +1100

    Error and exit from wrapper if executable is not available

--- bash/shellject_wrapper.sh
+++ bash/shellject_wrapper.sh
@@ -2,6 +2,11 @@ shellject ()
 {
   local executable output shellject_exit eval_exit
   executable=$( type -P shellject );
+  if [[ "$executable" == "" ]]; then
+    >&2 echo "shellject executable does not seem to exist."
+    >&2 echo "Make sure shellject gem is installed correctly for $(ruby -v)"
+    return 1
+  fi
   output=$( $executable $@ );
   shellject_exit=$?;
   if [[ $shellject_exit == 0 && "$1" == "load" && ! ($@ =~ "--help" || $@ =~ " -h") ]]; then
