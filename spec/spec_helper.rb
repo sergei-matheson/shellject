d85749154ec677b2ffddb63ce86d496e0845e8a2
Sergei Matheson <sergei.matheson@gmail.com>
2017-03-10 16:14:54 +1100

    Update codeclimate reporter usage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
 # At the top because simplecov needs to watch files being loaded
 require 'simplecov'
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
